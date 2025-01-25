8d6611f863cf3d896b24775c549d4de4f5fa0be6
Shravani <sawantshravani03@gmail.com>
2025-01-25 12:36:22 +0530

    Changed file structure

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    // root: 'portfolio', // Set the root to the portfolio folder
+    plugins: [react()],
+    // base:"shravani-portfolio", 
+    server: {
+        open: true, // Automatically open the browser
+    },
+});
